68de8db8fadc3adfd9a73195fc1a91a881c70e51
Fale7-Res <fale7resturant1961@gmail.com>
2025-07-06 06:21:27 +0300

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,16 @@
+import { views } from '../lib/views.js';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).send(views.login({ error: null }));
+  } else if (req.method === 'POST') {
+    if (req.body.password === 'fale71961') {
+      res.setHeader('Set-Cookie', 'auth=loggedIn; Path=/; HttpOnly; SameSite=Strict');
+      res.redirect(302, '/api/admin');
+    } else {
+      res.status(200).send(views.login({ error: 'كلمة المرور غير صحيحة' }));
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
